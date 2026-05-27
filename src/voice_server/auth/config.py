d0ca0c0338d1e1e1552c110a6420ed7e1617ea01
Simon Gent <sigent@amazon.co.uk>
2026-05-27 15:05:56 +0100

    style: apply ruff formatting to auth/config.py

--- src/voice_server/auth/config.py
+++ src/voice_server/auth/config.py
@@ -9,9 +9,7 @@ class AuthConfig:
     cognito_user_pool_id: str = field(
         default_factory=lambda: os.environ.get("COGNITO_USER_POOL_ID", "")
     )
-    cognito_client_id: str = field(
-        default_factory=lambda: os.environ.get("COGNITO_CLIENT_ID", "")
-    )
+    cognito_client_id: str = field(default_factory=lambda: os.environ.get("COGNITO_CLIENT_ID", ""))
     cognito_region: str = field(
         default_factory=lambda: os.environ.get("COGNITO_REGION", "eu-west-1")
     )
@@ -21,7 +19,9 @@ class AuthConfig:
 
     @property
     def issuer(self) -> str:
-        return f"https://cognito-idp.{self.cognito_region}.amazonaws.com/{self.cognito_user_pool_id}"
+        return (
+            f"https://cognito-idp.{self.cognito_region}.amazonaws.com/{self.cognito_user_pool_id}"
+        )
 
     @property
     def jwks_url(self) -> str:
